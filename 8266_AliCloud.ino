6df5b0a336b40415c5b1f31390ddadd029cae1e0
CaptainZZZ <33072544+CaptainZZZ@users.noreply.github.com>
2019-02-24 18:09:48 -0500

    Update 8266_AliCloud.ino

--- 8266_AliCloud.ino
+++ 8266_AliCloud.ino
@@ -1,14 +1,14 @@
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+//Wifi 配置
+#define WIFI_SSID         ""
+#define WIFI_PASSWD       ""
 
-#define WIFI_SSID         "RogersA0068"
-#define WIFI_PASSWD       "7EAB7Rogers"
-int SENSOR_PIN=10;
-/* 产品的三元组信息，根据9个测试设备的三元组，每个设备都烧录不同的*/
-#define PRODUCT_KEY       "a1HtLanBEMH"
-#define DEVICE_NAME       "ESP8266_001"
-#define DEVICE_SECRET     "YpWkcNpTUxFFBXvHgsmKBwiC47xom4mc"
+/* 产品的三元组信息*/
+#define PRODUCT_KEY       ""
+#define DEVICE_NAME       ""
+#define DEVICE_SECRET     ""
 #define REGION_ID          "cn-shanghai"
 /* LD线上环境域名和端口号，不需要改 */
 #define MQTT_SERVER       PRODUCT_KEY ".iot-as-mqtt." REGION_ID ".aliyuncs.com"
@@ -17,12 +17,12 @@ int SENSOR_PIN=10;
 
 // TODO: MQTT连接的签名信息，哈希加密请以"clientIdtestdeviceName"+设备名称+"productKey"+设备模型标识+“timestamp123456789”前往http://tool.oschina.net/encrypt?type=2进行加密
 #define CLIENT_ID         "12345|securemode=3,signmethod=hmacsha1,timestamp=123456789|"
-#define MQTT_PASSWD       "a07778bf512d2e5748de5de44b625105ec4c9df3"
+#define MQTT_PASSWD       ""
 
 #define ALINK_BODY_FORMAT         "{\"id\":\"12345\",\"method\":\"thing.event.property.post\",\"params\":%s}"
 #define ALINK_TOPIC_PROP_POST     "/sys/"PRODUCT_KEY"/"DEVICE_NAME"/thing/event/property/post"
 //==========================================
-//Lib parameters update
+//PubSubClient.h库参数修改，否则连不上
 // MQTT_MAX_PACKET_SIZE : Maximum packet size
 //#define MQTT_MAX_PACKET_SIZE 1024
 // MQTT_KEEPALIVE : keepAlive interval in Seconds
@@ -77,37 +77,34 @@ void mqttCheckConnect()
         }
     }
 }
-
+//Publish消息，
 void mqttIntervalPost()
 {
     char param[200];
     char jsonBuf[1000];
     int x=150;
-    sprintf(param, "{\"PM25Value\":%d}",x);
+    sprintf(param, "{\"PM25Value\":%d}",x); //此处填写属性的标识符，例如PM25Value
     sprintf(jsonBuf, ALINK_BODY_FORMAT, param);
     Serial.println(jsonBuf);
     boolean d = client.publish(ALINK_TOPIC_PROP_POST, jsonBuf);
-    Serial.println("publish:0 失败;1成功");
+    Serial.println("publish:0=Failed;1=Success");
     Serial.println(d);
 }
 
 void setup() 
 {
-    pinMode(13,  INPUT);
     /* initialize serial for debugging */
     Serial.begin(115200);
-    Serial.println("Demo Start");
     wifiInit();
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-    if (millis() - lastMs >= 10000)
+    if (millis() - lastMs >= 10000)//每10秒publish一下
     {
         lastMs = millis();
         mqttCheckConnect(); 
-        /* 上报消息心跳周期 */
         mqttIntervalPost();
     }
     client.loop();
